66c1af98a0c4ad5998a60fc61c78e97214a6164a
Bianca Alessandra Levandowski <160682561+Bialevandowski@users.noreply.github.com>
2024-02-20 22:22:47 -0300

    Update atividade1.java

--- atividade1.java
+++ atividade1.java
@@ -1,1 +1,10 @@
+public class Variaveis {
+    public static void main(String[] args) {
+        int idade = 30;
+        double altura = 1.75;
+        char genero = 'M';
+        boolean temCarteiraDeMotorista = true;
 
+    
+    }
+}
