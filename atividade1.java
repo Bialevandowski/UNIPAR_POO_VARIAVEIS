bcfd701d0691cbceda60a5d9b8418534b0727db7
Bianca Alessandra Levandowski <160682561+Bialevandowski@users.noreply.github.com>
2024-02-20 22:28:26 -0300

    Update atividade1.java

--- atividade1.java
+++ atividade1.java
@@ -1,10 +1,18 @@
 public class Variaveis {
     public static void main(String[] args) {
-        int idade = 30;
-        double altura = 1.75;
-        char genero = 'M';
+        int idade =18;
+        double altura = 1.73;
+        char genero = 'F';
         boolean temCarteiraDeMotorista = true;
-
+        float 
+        long
+        short
+        byte
+        char
+System.out.println("Idade: " + idade);
+        System.out.println("Altura: " + altura);
+        System.out.println("Gênero: " + genero);
+        System.out.println("Tem carteira de motorista? " + temCarteiraDeMotorista);
     
     }
 }
