46f46cee774ca170cec292db790b8695fa86da8d
Bianca Alessandra Levandowski <160682561+Bialevandowski@users.noreply.github.com>
2024-03-04 09:37:24 -0300

    atividade2.java

---
+++ atividade2.java
@@ -0,0 +1,35 @@
+
+    public class Main {
+    public static void main(String[] args) {
+       
+     
+        String nomeProduto1;
+
+       
+        double precoProduto;
+
+       
+        String marcaDoProduto;
+
+      
+        int quantidadeProduto;
+
+      
+        String descricaoProduto;
+
+     
+        String nomeProduto;
+
+       
+        int numeroProduto123;
+
+      
+        double precoDoProduto;
+
+     
+        int estoqueDisponivel;
+
+      
+        int totalDeProdutos;
+    }
+}
