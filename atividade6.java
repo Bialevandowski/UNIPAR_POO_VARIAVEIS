d261d9843be6f7303c482799c1909db826ff5a28
Bianca Alessandra Levandowski <160682561+Bialevandowski@users.noreply.github.com>
2024-03-04 19:17:56 -0300

    Update atividade6.java

--- atividade6.java
+++ atividade6.java
@@ -1,1 +1,17 @@
-
+public class Main {
+    public static void main(String[] args) {
+       
+        Double numeroProduto = "0"; 
+        double precoProduto = "";
+        String descricaoProduto = "" ;
+        
+        System.out.println("Descricao do Produto: " + descricaoProduto);
+        
+        descricaoProduto = "Fone de ouvido Bluetooth";
+        
+        System.out.println("Dados do Produto:");
+        System.out.println("Numero: " + numeroProduto);
+        System.out.println("Preco: $" + precoProduto);
+        System.out.println("Descricao: " + descricaoProduto);
+    }
+}
