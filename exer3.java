94ff29a1cad5b01708ca656f5c9d24bc9e56dbc9
Bianca Alessandra Levandowski <160682561+Bialevandowski@users.noreply.github.com>
2024-03-04 21:57:24 -0300

    Update exer3.java

--- exer3.java
+++ exer3.java
@@ -1,1 +1,29 @@
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) {
+        
+        int primeiroValor = 0;
+        int segundoValor = 0;
+         int terceiroValor = 0;
+        int valorTotal = 0;
+        Scanner scanner = new Scanner(System.in);
+        
+        
+        System.out.println("informe o primeiro valor");
+       primeiroValor = scanner.nextInt();
+       System.out.println("informe o segundo valor");
+       segundoValor = scanner.nextInt();
+       System.out.println("informe o terceiro valor");
+       terceiroValor = scanner.nextInt();
+        
+       valorTotal = (primeiroValor + segundoValor) + terceiroValor; 
+       
+       
+       
+        scanner.close();
+        
+        
+        System.out.println("O valor total eh: " + valorTotal); 
+    }
+}
