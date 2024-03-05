41df0ae934b2247d3202cc1c9d6ce45a33958f48
Bianca Alessandra Levandowski <160682561+Bialevandowski@users.noreply.github.com>
2024-03-04 22:00:55 -0300

    Update 2exer3.java

--- 2exer3.java
+++ 2exer3.java
@@ -1,1 +1,26 @@
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) {
+        
+        int primeiroValor = 0;
+        int segundoValor = 0;
+        int valorTotal = 0;
+        Scanner scanner = new Scanner(System.in);
+        
+        
+        System.out.println("informe o primeiro valor");
+       primeiroValor = scanner.nextInt();
+       System.out.println("informe o segundo valor");
+       segundoValor = scanner.nextInt();
+      
+       valorTotal = primeiroValor - segundoValor; 
+       
+       
+       
+        scanner.close();
+        
+        
+        System.out.println("O valor total eh: " + valorTotal); 
+    }
+}
